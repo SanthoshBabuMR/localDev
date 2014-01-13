5aca293b52e056a57bc4d3c6194135c644b2e6e5
santhosh <html5webster@gmail.com>
2014-01-13 21:48:13 +0530

    added jasmine and my first test spec doc(s)

---
+++ WebContent/src/doc/my-first-test-spec/spec/hello_spec.js
@@ -0,0 +1,9 @@
+// "Hello World" suite
+describe("Hello World", function() {
+  
+  // specification
+  it("says hello", function() {
+    expect(helloWorld()).toEqual("Hello World");  
+  });
+  
+});
